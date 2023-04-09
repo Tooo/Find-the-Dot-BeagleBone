96108acf925bcd62d93d2e14ab5430321a1d2049
Travis <Toooo123@live.ca>
2023-04-09 15:01:46 -0700

    Add playsound logic

--- linux-code/buzzerPlayer.c
+++ linux-code/buzzerPlayer.c
@@ -9,9 +9,36 @@ static pthread_t buzzerThread;
 static void* BuzzerPlayer_threadFunction(void* args);
 static bool stopping;
 
+#define SOUND_LENGTH 5
+#define SOUND_ARG_LENGTH 2
+
+static int (*currentSound)[SOUND_ARG_LENGTH];
+static int playIndex;
+static bool newSound;
+
+// Pattern for hit
+static int hitSound[SOUND_LENGTH][SOUND_ARG_LENGTH] = {
+    {(int)BUZZER_NOTE_A, 500}, 
+    {(int)BUZZER_NOTE_B, 500}, 
+    {(int)BUZZER_NOTE_C, 500}, 
+    {(int)BUZZER_NOTE_D, 500},
+    {(int)BUZZER_NOTE_D, 500}
+};
+
+// Pattern for miss
+static int missSound[SOUND_LENGTH][SOUND_ARG_LENGTH] = {
+    {(int)BUZZER_NOTE_D, 500}, 
+    {(int)BUZZER_NOTE_A, 500}, 
+    {(int)BUZZER_NOTE_D, 500}, 
+    {(int)BUZZER_NOTE_A, 500},
+    {(int)BUZZER_NOTE_D, 500}
+};
+
 void BuzzerPlayer_init(void)
 {
     stopping = false;
+    playIndex = SOUND_LENGTH;
+    newSound = false;
     pthread_create(&buzzerThread, NULL, &BuzzerPlayer_threadFunction, NULL);
 }
 
@@ -21,11 +48,30 @@ void BuzzerPlayer_cleanup(void)
     pthread_join(buzzerThread, NULL);
 }
 
+void BuzzerPlayer_playSound(BuzzerPlayerEnum sound)
+{
+    if (sound == BUZZER_PLAYER_HIT) {
+        currentSound = hitSound;
+    } else if (sound == BUZZER_PLAYER_MISS) {
+        currentSound = missSound;
+    }
+    newSound = true;
+    playIndex = 0;
+}
+
 static void* BuzzerPlayer_threadFunction(void* args)
 {
     (void)args;
     while (!stopping) {
-
+        while (playIndex < SOUND_LENGTH) {
+            Buzzer_playNote(currentSound[playIndex][0], currentSound[playIndex][1]);
+            
+            if (!newSound) {
+                playIndex++;
+            } else {
+                newSound = false;
+            }
+        }
     }
     return NULL;
 }
--- linux-code/buzzerPlayer.h
+++ linux-code/buzzerPlayer.h
@@ -3,8 +3,17 @@
 #ifndef BUZZER_PLAYER_H_
 #define BUZZER_PLAYER_H_
 
+// Buzzer sounds
+typedef enum {
+    BUZZER_PLAYER_HIT = 0,
+    BUZZER_PLAYER_MISS,
+    BUZZER_PLAYER_COUNT,
+} BuzzerPlayerEnum;
+
 // Initialize/cleanup the module's data structures.
 void BuzzerPlayer_init(void);
 void BuzzerPlayer_cleanup(void);
+
+void BuzzerPlayer_playSound(BuzzerPlayerEnum sound);
 
 #endif
